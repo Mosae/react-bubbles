789d0ba152b7ca1ede8b3ee272f7cbf59bd088cf
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-24 10:51:57 -0500

    Handle change + initial state done in login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import AxiosWithAuth from '../utils/AxiosWithAuth';
 
-const Login = () => {
+const initialState = {
+	username: '',
+	password: '',
+	isFetching: false,
+};
+const Login = (props) => {
+	const [login, setLogin] = useState(initialState);
 	// make a post request to retrieve a token from the api
 	// when you have handled the token, navigate to the BubblePage route
+
+	const handleChange = (e) => {
+		setLogin({ ...login, [e.target.name]: e.target.value });
+	};
 	return <></>;
 };
 
