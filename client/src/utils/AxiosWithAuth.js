76b4931b4247635fad7d45770546644475036bb3
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-24 10:44:39 -0500

    BubblePage has useeffect- logs auth error - port 5000 - 404

--- client/src/utils/AxiosWithAuth.js
+++ client/src/utils/AxiosWithAuth.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-export const axiosWithAuth = () => {
+const axiosWithAuth = () => {
 	const token = localStorage.getItem('token');
 	return axios.create({
-		baseURL: ``,
+		baseURL: `http://localhost:5000`,
 		header: {
 			Authorization: token,
 		},
 	});
 };
+export default axiosWithAuth;
