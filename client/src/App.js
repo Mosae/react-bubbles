6c8b10dd2a63768fe43770c7ee8a3b0119bc8602
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-24 10:26:40 -0500

    Imported private route + bubble page to app.js and protected it

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import './styles.scss';
+import PrivateRoute from './components/PrivateRoute';
+//components
+import BubblePage from './components/BubblePage';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-      </div>
-    </Router>
-  );
+				<PrivateRoute path="bubbles-page" component={BubblePage} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
